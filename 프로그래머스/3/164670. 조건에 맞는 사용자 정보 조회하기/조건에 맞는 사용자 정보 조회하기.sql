12f7285cbd73737ec12aee500dfc8a3eac5103c5
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2024-04-04 15:37:43 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -4,11 +4,31 @@
 -- 주소는 띄어쓰기로 구분된 전체 주소, 전화번호는 하이픈으로 구분된 전체 전화번호
 -- ID 기준 내림차순
 
-# 풀이 1. JOIN 없음, 서브쿼리, INSERT 사용
+# INSERT(original_string, position, length, new_string)
+# original_string은 원본 문자열.
+# position은 new_string이 삽입된 후의 위치.
+# length는 original_string에서 new_string 삽입 시 제거될 문자 개수.
+# new_string은 삽입될 문자열.
+
+# 풀이 1. JOIN 없음, 서브쿼리 / INSERT 사용
+# SELECT USER_ID
+#      , NICKNAME
+#      , CONCAT(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) AS '전체주소'
+#      , INSERT(INSERT(TLNO, 8, 0, '-'), 4, 0, '-') AS '전화번호'
+#     FROM USED_GOODS_USER
+#     WHERE USER_ID IN (
+#         SELECT WRITER_ID
+#             FROM USED_GOODS_BOARD
+#             GROUP BY WRITER_ID
+#             HAVING COUNT(BOARD_ID) >= 3
+#         )
+#     ORDER BY USER_ID DESC
+
+# 풀이 2. JOIN 없음, 서브쿼리 / REGEXP_REPLACE 사용 
 SELECT USER_ID
      , NICKNAME
      , CONCAT(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) AS '전체주소'
-     , INSERT(INSERT(TLNO, 8, 0, '-'), 4, 0, '-') AS '전화번호'
+     , REGEXP_REPLACE(TLNO, '(.{3})(.{4})(.{4})', '$1-$2-$3') AS '전화번호'
     FROM USED_GOODS_USER
     WHERE USER_ID IN (
         SELECT WRITER_ID

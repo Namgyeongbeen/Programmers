84f020d422cafbea86ca30ae5c1d7095c5306a54
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2024-07-04 01:31:47 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,23 +1,24 @@
-# -- 문제에 상/하반기 관련 조건이 없는데, 상하반기 점수의 평균으로 하자.
-# -- 썩 좋은 문제는 아닌 듯. 조건이 명확하지 않아 푸는 사람들이 추측하게 하는 문제다.
-
-SELECT S.EMP_NO
+-- 사번, 성명, 평가등급, 성과금 조회.
+-- 평가등급은 GRADE, 성과금은 BONUS
+-- 문제에 상/하반기 관련 조건이 없는데, 상하반기 점수의 평균으로 하자.
+-- 썩 좋은 문제는 아닌 듯. 조건이 명확하지 않아 푸는 사람들이 추측하게 하는 문제다.
+    
+SELECT E.EMP_NO
      , E.EMP_NAME
-     , CASE WHEN S.SCORE >= 96 THEN 'S'
-            WHEN S.SCORE >= 90 THEN 'A'
-            WHEN S.SCORE >= 80 THEN 'B'
-            ELSE 'C'
+     , CASE WHEN G.SCORE < 80 THEN 'C'
+            WHEN G.SCORE >= 80 AND G.SCORE < 90 THEN 'B'
+            WHEN G.SCORE >= 90 AND G.SCORE < 96 THEN 'A'
+            ELSE 'S'
             END AS GRADE
-     , CASE WHEN S.SCORE >= 96 THEN E.SAL*0.2
-            WHEN S.SCORE >= 90 THEN E.SAL*0.15
-            WHEN S.SCORE >= 80 THEN E.SAL*0.1
-            ELSE 0
+     , CASE WHEN G.SCORE < 80 THEN 0
+            WHEN G.SCORE >= 80 AND G.SCORE < 90 THEN E.SAL * 0.1
+            WHEN G.SCORE >= 90 AND G.SCORE < 96 THEN E.SAL * 0.15
+            ELSE E.SAL * 0.2
             END AS BONUS
     FROM (
-        SELECT EMP_NO
-             , AVG(SCORE) AS SCORE
+        SELECT EMP_NO, AVG(SCORE) AS SCORE
             FROM HR_GRADE
             GROUP BY EMP_NO
-         ) AS S
+    ) AS G
     LEFT JOIN HR_EMPLOYEES E
-        ON S.EMP_NO = E.EMP_NO
+        ON G.EMP_NO = E.EMP_NO

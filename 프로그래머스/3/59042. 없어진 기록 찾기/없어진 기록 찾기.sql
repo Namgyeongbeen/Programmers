9070e32e47c81ea328150e0456df8a0820e7e27e
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2024-07-05 11:58:05 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,8 +1,10 @@
--- 코드를 입력하세요
+-- 입양 간 기록은 있으나 보호소 입소 기록이 없는 동물들의 ID와 이름 조회
+-- ID순 정렬
+
 SELECT O.ANIMAL_ID
      , O.NAME
     FROM ANIMAL_OUTS O
     LEFT JOIN ANIMAL_INS I
         ON O.ANIMAL_ID = I.ANIMAL_ID
-    WHERE I.ANIMAL_TYPE IS NULL
+    WHERE I.ANIMAL_ID IS NULL
     ORDER BY O.ANIMAL_ID

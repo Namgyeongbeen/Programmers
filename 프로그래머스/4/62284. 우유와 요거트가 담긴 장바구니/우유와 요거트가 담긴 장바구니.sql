ecc0220dcf15536a14cb337f2e99a37b0bd9c345
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2024-07-05 16:47:09 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,7 +1,9 @@
--- 코드를 입력하세요       
+-- 우유와 요거트를 동시에 구입한 장바구니의 ID 조회
+-- ID순 정렬
+
 SELECT CART_ID
 FROM CART_PRODUCTS
 WHERE NAME IN ('Milk', 'Yogurt')
 GROUP BY CART_ID
-HAVING COUNT(DISTINCT NAME) = 2
+HAVING COUNT(DISTINCT NAME) >= 2
 ORDER BY CART_ID

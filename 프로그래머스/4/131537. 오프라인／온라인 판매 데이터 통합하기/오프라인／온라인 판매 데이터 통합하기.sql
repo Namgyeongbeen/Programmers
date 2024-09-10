58cc2d098f13643b0ee741e8a624cdcfefef312a
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2024-09-10 15:17:35 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,14 +1,52 @@
 -- 코드를 입력하세요
 SELECT *
-    FROM 
-    (
-        SELECT DATE_FORMAT(O1.SALES_DATE, '%Y-%m-%d') AS SALES_DATE
-             , O1.PRODUCT_ID, O1.USER_ID, O1.SALES_AMOUNT
-            FROM ONLINE_SALE O1
+    FROM ( 
+        SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS SALES_DATE
+             , PRODUCT_ID
+             , USER_ID
+             , SALES_AMOUNT 
+            FROM ONLINE_SALE
         UNION ALL
-        SELECT DATE_FORMAT(O2.SALES_DATE, '%Y-%m-%d') AS SALES_DATE
-             , O2.PRODUCT_ID, NULL AS USER_ID, O2.SALES_AMOUNT
-            FROM OFFLINE_SALE O2
-        ) B
-    WHERE B.SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31'
-    ORDER BY B.SALES_DATE, B.PRODUCT_ID, B.USER_ID
+        SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS SALES_DATE
+             , PRODUCT_ID
+             , NULL AS USER_ID
+             , SALES_AMOUNT
+            FROM OFFLINE_SALE
+    ) AS BASE
+    WHERE SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31'
+    ORDER BY SALES_DATE, PRODUCT_ID, USER_ID
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT *
+#     FROM 
+#     (
+#         SELECT DATE_FORMAT(O1.SALES_DATE, '%Y-%m-%d') AS SALES_DATE
+#              , O1.PRODUCT_ID, O1.USER_ID, O1.SALES_AMOUNT
+#             FROM ONLINE_SALE O1
+#         UNION ALL
+#         SELECT DATE_FORMAT(O2.SALES_DATE, '%Y-%m-%d') AS SALES_DATE
+#              , O2.PRODUCT_ID, NULL AS USER_ID, O2.SALES_AMOUNT
+#             FROM OFFLINE_SALE O2
+#         ) B
+#     WHERE B.SALES_DATE BETWEEN '2022-03-01' AND '2022-03-31'
+#     ORDER BY B.SALES_DATE, B.PRODUCT_ID, B.USER_ID

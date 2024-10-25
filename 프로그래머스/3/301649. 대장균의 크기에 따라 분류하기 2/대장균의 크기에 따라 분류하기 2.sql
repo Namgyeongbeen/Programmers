574678350262ec7643a724f9d5211e3e68299bfe
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2024-10-25 14:42:55 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,4 +1,19 @@
 -- 코드를 작성해주세요
+# SELECT ID
+#      , CASE WHEN PERCENT <= 0.25 THEN 'CRITICAL'
+#             WHEN PERCENT <= 0.5 THEN 'HIGH'
+#             WHEN PERCENT <= 0.75 THEN 'MEDIUM'
+#             ELSE 'LOW'
+#             END AS COLONY_NAME
+#     FROM (
+#         SELECT ID
+#              , SIZE_OF_COLONY
+#              , PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY DESC) AS PERCENT
+#             FROM ECOLI_DATA
+#         ) B
+#     ORDER BY ID
+
+
 SELECT ID
      , CASE WHEN PERCENT <= 0.25 THEN 'CRITICAL'
             WHEN PERCENT <= 0.5 THEN 'HIGH'
@@ -8,7 +23,26 @@ SELECT ID
     FROM (
         SELECT ID
              , SIZE_OF_COLONY
-             , PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY DESC) AS PERCENT
+             , PERCENT_RANK() OVER (ORDER BY SIZE_OF_COLONY DESC) AS PERCENT
             FROM ECOLI_DATA
-        ) B
+        ) AS B
     ORDER BY ID
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         SELECT ID
+#              , SIZE_OF_COLONY
+#              , PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY DESC) AS PERCENT
+#             FROM ECOLI_DATA

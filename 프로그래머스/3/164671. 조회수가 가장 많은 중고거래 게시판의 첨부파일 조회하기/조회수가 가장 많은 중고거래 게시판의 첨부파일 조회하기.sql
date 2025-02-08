2f6d8c948584362aefd4b8e76add30ad5e558274
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2025-02-08 19:45:20 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,14 +1,12 @@
--- 코드를 입력하세요
+-- 조회수가 가장 높은 중고거래 게시물에 대한 첨부파일 경로 출력, FILE ID 기준 내림차순
 -- 파일경로 : /home/grep/src/BOARD_ID
 -- 파일 이름 : 파일ID, 파일이름, 파일확장자로 구성됨
 -- 조회수가 가장 높은 게시물은 유일함.
 
-SELECT CONCAT('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
-    FROM USED_GOODS_FILE
-    WHERE BOARD_ID = (
-        SELECT BOARD_ID 
-            FROM USED_GOODS_BOARD 
-            ORDER BY VIEWS DESC
-            LIMIT 1
-        )
+-- 25.02.08
+SELECT CONCAT('/home/grep/src/', B.BOARD_ID, '/', F.FILE_ID, F.FILE_NAME, F.FILE_EXT) AS FILE_PATH
+    FROM USED_GOODS_BOARD B
+    LEFT JOIN USED_GOODS_FILE F
+        ON B.BOARD_ID = F.BOARD_ID
+    WHERE VIEWS = (SELECT MAX(VIEWS) FROM USED_GOODS_BOARD)
     ORDER BY FILE_ID DESC

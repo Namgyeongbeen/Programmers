8f391be4e3f0a74c15ff574d81ed4da44096241c
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2025-02-12 15:22:25 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,9 +1,30 @@
 -- 코드를 입력하세요
 -- 입양 간 동물 중 보호기간이 가장 길었던 동물 2마리의 아이디와 이름 조회
 -- 보호기간 긴 순으로 조회
-SELECT I.ANIMAL_ID, I.NAME
+
+-- 25.02.12
+SELECT I.ANIMAL_ID
+     , I.NAME
     FROM ANIMAL_INS I
     JOIN ANIMAL_OUTS O
         ON I.ANIMAL_ID = O.ANIMAL_ID
     ORDER BY TIMESTAMPDIFF(MINUTE, I.DATETIME, O.DATETIME) DESC
     LIMIT 2
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT I.ANIMAL_ID, I.NAME
+#     FROM ANIMAL_INS I
+#     JOIN ANIMAL_OUTS O
+#         ON I.ANIMAL_ID = O.ANIMAL_ID
+#     ORDER BY TIMESTAMPDIFF(MINUTE, I.DATETIME, O.DATETIME) DESC
+#     LIMIT 2

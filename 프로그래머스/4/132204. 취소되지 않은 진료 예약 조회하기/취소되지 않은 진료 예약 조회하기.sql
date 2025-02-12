dcce6ce45c5112bdc84d2c816d0a10da15488e39
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2025-02-13 01:43:56 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -2,21 +2,22 @@
 -- APNT_NO, PT_NAME, PT_NO, MCDP_CD, DR_NAME, APNT_YMD
 -- 진료예약일시를 기준으로 오름차순 정렬
 
+-- 25.02.13
 SELECT A.APNT_NO
      , P.PT_NAME
      , A.PT_NO
-     , D.MCDP_CD
+     , A.MCDP_CD
      , D.DR_NAME
      , A.APNT_YMD
     FROM APPOINTMENT A
-    LEFT JOIN PATIENT P
+    JOIN PATIENT P
         ON A.PT_NO = P.PT_NO
-    LEFT JOIN DOCTOR D
+    JOIN DOCTOR D
         ON A.MDDR_ID = D.DR_ID
-    WHERE A.APNT_CNCL_YN = 'N'
-        AND A.APNT_YMD BETWEEN '2022-04-13 00:00:00' AND '2022-04-13 23:59:59'
+    WHERE DATE_FORMAT(A.APNT_YMD, '%Y-%m-%d') = '2022-04-13'
+        AND A.APNT_CNCL_YN = 'N'
         AND A.MCDP_CD = 'CS'
-    ORDER BY APNT_YMD
+    ORDER BY A.APNT_YMD
 
 
 
@@ -29,21 +30,21 @@ SELECT A.APNT_NO
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# SELECT A.APNT_NO
+#      , P.PT_NAME
+#      , A.PT_NO
+#      , D.MCDP_CD
+#      , D.DR_NAME
+#      , A.APNT_YMD
+#     FROM APPOINTMENT A
+#     LEFT JOIN PATIENT P
+#         ON A.PT_NO = P.PT_NO
+#     LEFT JOIN DOCTOR D
+#         ON A.MDDR_ID = D.DR_ID
+#     WHERE A.APNT_CNCL_YN = 'N'
+#         AND A.APNT_YMD BETWEEN '2022-04-13 00:00:00' AND '2022-04-13 23:59:59'
+#         AND A.MCDP_CD = 'CS'
+#     ORDER BY APNT_YMD
 
 
 # SELECT A.APNT_NO

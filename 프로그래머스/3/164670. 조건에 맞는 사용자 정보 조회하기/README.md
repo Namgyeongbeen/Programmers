9d4499d1351defa1aefdfd494cc8cb2913651ce9
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2025-02-12 13:45:54 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 조건에 맞는 사용자 정보 조회하기 - 164670 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164670) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 04월 04일 15:37:42
+2025년 02월 12일 13:45:52
 
 ### 문제 설명
 
--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -10,7 +10,31 @@
 # length는 original_string에서 new_string 삽입 시 제거될 문자 개수.
 # new_string은 삽입될 문자열.
 
-# 풀이 1. JOIN 없음, 서브쿼리 / INSERT 사용
+-- 25.02.12
+-- 1. 게시물 3건 이상 사용한 등록자 찾기
+-- 2. 그 사용자의 NICKNAME, 주소, 전화번호 출력
+
+WITH USERS_THREE_TITLE AS (
+    SELECT *
+        FROM USED_GOODS_BOARD
+        GROUP BY WRITER_ID
+        HAVING COUNT(BOARD_ID) >= 3
+)
+SELECT USER_ID
+     , NICKNAME
+     , CONCAT(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) AS "전체주소"
+     , INSERT(INSERT(TLNO, 4, 0, '-'), 9, 0, '-') AS "전화번호"
+    FROM USED_GOODS_USER
+    WHERE USER_ID IN (SELECT WRITER_ID FROM USERS_THREE_TITLE)
+    ORDER BY USER_ID DESC
+
+
+
+
+
+
+
+# # 풀이 1. JOIN 없음, 서브쿼리 / INSERT 사용
 # SELECT USER_ID
 #      , NICKNAME
 #      , CONCAT(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) AS '전체주소'
@@ -24,16 +48,16 @@
 #         )
 #     ORDER BY USER_ID DESC
 
-# 풀이 2. JOIN 없음, 서브쿼리 / REGEXP_REPLACE 사용 
-SELECT USER_ID
-     , NICKNAME
-     , CONCAT(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) AS '전체주소'
-     , REGEXP_REPLACE(TLNO, '(.{3})(.{4})(.{4})', '$1-$2-$3') AS '전화번호'
-    FROM USED_GOODS_USER
-    WHERE USER_ID IN (
-        SELECT WRITER_ID
-            FROM USED_GOODS_BOARD
-            GROUP BY WRITER_ID
-            HAVING COUNT(BOARD_ID) >= 3
-        )
-    ORDER BY USER_ID DESC
+# # 풀이 2. JOIN 없음, 서브쿼리 / REGEXP_REPLACE 사용 
+# SELECT USER_ID
+#      , NICKNAME
+#      , CONCAT(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) AS '전체주소'
+#      , REGEXP_REPLACE(TLNO, '(.{3})(.{4})(.{4})', '$1-$2-$3') AS '전화번호'
+#     FROM USED_GOODS_USER
+#     WHERE USER_ID IN (
+#         SELECT WRITER_ID
+#             FROM USED_GOODS_BOARD
+#             GROUP BY WRITER_ID
+#             HAVING COUNT(BOARD_ID) >= 3
+#         )
+#     ORDER BY USER_ID DESC

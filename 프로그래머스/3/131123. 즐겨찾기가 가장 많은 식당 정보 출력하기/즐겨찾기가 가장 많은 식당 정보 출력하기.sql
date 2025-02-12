5871a00b96739b886e65eef4410b69b7e4492a8e
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2025-02-12 14:54:32 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,18 +1,43 @@
--- 음식 종류별 즐겨찾기가 가장 많은 식당의 음식 종류, ID, 이름, 즐겨찾기 수 조회
+-- 음식 종류별 즐겨찾기가 가장 많은 식당의 FOOD_TYPE, REST_ID, REST_NAME, FAVORITES 조회
 -- 음식 종류 기준 내림차순 정렬
+
+-- 25.02.12
+WITH FAVORITES_RANK AS (
+    SELECT *
+         , DENSE_RANK() OVER(PARTITION BY FOOD_TYPE ORDER BY FAVORITES DESC) AS RANKS
+        FROM REST_INFO
+)
 
 SELECT FOOD_TYPE
      , REST_ID
      , REST_NAME
      , FAVORITES
-    FROM REST_INFO
-    WHERE (FOOD_TYPE, FAVORITES) IN (
-        SELECT FOOD_TYPE
-             , MAX(FAVORITES)
-            FROM REST_INFO
-            GROUP BY FOOD_TYPE
-    )
+    FROM FAVORITES_RANK
+    WHERE RANKS = 1
     ORDER BY FOOD_TYPE DESC
+
+
+
+
+
+
+
+
+
+
+
+# SELECT FOOD_TYPE
+#      , REST_ID
+#      , REST_NAME
+#      , FAVORITES
+#     FROM REST_INFO
+#     WHERE (FOOD_TYPE, FAVORITES) IN (
+#         SELECT FOOD_TYPE
+#              , MAX(FAVORITES)
+#             FROM REST_INFO
+#             GROUP BY FOOD_TYPE
+#     )
+#     ORDER BY FOOD_TYPE DESC
     
 
 

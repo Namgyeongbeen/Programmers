f9716e2de6faa27433352206e3f4568778a5d3b6
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2025-02-12 15:04:38 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,16 +1,18 @@
--- 공간을 둘 이상 등록한 사람 : 헤비유저
+-- 공간을 2군데 이상 등록한 사람 : 헤비유저
 -- 헤비유저가 등록한 공간의 정보를 ID순으로 조회.
 
+-- 25.02.12
+WITH HEAVY_USERS AS (
+    SELECT HOST_ID
+        FROM PLACES
+        GROUP BY HOST_ID
+        HAVING COUNT(HOST_ID) >= 2
+)
 SELECT ID
      , NAME
      , HOST_ID
     FROM PLACES
-    WHERE HOST_ID IN (
-        SELECT HOST_ID
-            FROM PLACES
-            GROUP BY HOST_ID
-            HAVING COUNT(ID) >= 2
-    )
+    WHERE HOST_ID IN (SELECT * FROM HEAVY_USERS)
     ORDER BY ID
 
 
@@ -23,21 +25,17 @@ SELECT ID
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# SELECT ID
+#      , NAME
+#      , HOST_ID
+#     FROM PLACES
+#     WHERE HOST_ID IN (
+#         SELECT HOST_ID
+#             FROM PLACES
+#             GROUP BY HOST_ID
+#             HAVING COUNT(ID) >= 2
+#     )
+#     ORDER BY ID
 
 
 # SELECT ID

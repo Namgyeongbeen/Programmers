bcb9b613c36d2f88cf5b29d5104ccf890da73737
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2025-02-13 14:10:12 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,18 +1,20 @@
 -- 서울에 위치한 식당들의 식당 ID, 식당 이름, 음식 종류, 즐겨찾기수, 주소, 리뷰 평균 점수를 조회
 -- 리뷰 평균점수는 소수점 세 번째 자리에서 반올림, 결과는 평균점수를 기준으로 내림차순 정렬, 평균점수가 같다면 즐겨찾기수를 기준으로 내림차순 정렬
 
-SELECT R1.REST_ID
-     , R1.REST_NAME
-     , R1.FOOD_TYPE
-     , R1.FAVORITES
-     , R1.ADDRESS
-     , ROUND(AVG(R2.REVIEW_SCORE), 2) AS SCORE
-    FROM REST_INFO R1
-    JOIN REST_REVIEW R2
-        ON R1.REST_ID = R2.REST_ID
+-- 25.02.13
+WITH AVG_SCORE AS (
+    SELECT REST_ID
+         , ROUND(AVG(REVIEW_SCORE), 2) AS SCORE
+        FROM REST_REVIEW
+        GROUP BY REST_ID
+)
+SELECT I.REST_ID, I.REST_NAME, I.FOOD_TYPE
+     , I.FAVORITES, I.ADDRESS, S.SCORE
+    FROM REST_INFO I
+    JOIN AVG_SCORE S
+        ON I.REST_ID = S.REST_ID
     WHERE ADDRESS LIKE '서울%'
-    GROUP BY R2.REST_ID
-    ORDER BY SCORE DESC, R1.FAVORITES DESC
+    ORDER BY SCORE DESC, FAVORITES DESC
 
 
 
@@ -23,14 +25,18 @@ SELECT R1.REST_ID
 
 
 
-
-
-
-
-
-
-
-
+# SELECT R1.REST_ID
+#      , R1.REST_NAME
+#      , R1.FOOD_TYPE
+#      , R1.FAVORITES
+#      , R1.ADDRESS
+#      , ROUND(AVG(R2.REVIEW_SCORE), 2) AS SCORE
+#     FROM REST_INFO R1
+#     JOIN REST_REVIEW R2
+#         ON R1.REST_ID = R2.REST_ID
+#     WHERE ADDRESS LIKE '서울%'
+#     GROUP BY R2.REST_ID
+#     ORDER BY SCORE DESC, R1.FAVORITES DESC
 
 # SELECT I.REST_ID, I.REST_NAME
 #      , I.FOOD_TYPE, I.FAVORITES, I.ADDRESS
